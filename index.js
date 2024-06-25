c61874ccf0a3c2bb2b085b006d867b7e673d9eb8
Aksparihar <141713750+Aksparihar@users.noreply.github.com>
2024-06-25 13:43:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000;
 
 app.get('/', (req,res)=>{
-app.res("hello world")
+res.send("hello world")
 })
 app.listen(port,()=>{
 console.log(`listning on port ${port}`)
-})
+});
